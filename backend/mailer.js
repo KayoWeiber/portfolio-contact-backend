b4063dc4fe44415fd5dcdb6a6847355fe2a2725e
Kayo Weiber <caioveiber598@gmail.com>
2025-10-03 15:05:35 -0300

    fix: correção envio de email

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,5 +1,10 @@
 import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
 
+dotenv.config();
+
+console.log("USER:", process.env.EMAIL_USER);
+console.log("PASS:", process.env.EMAIL_PASS ? "OK" : "MISSING");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -7,32 +12,38 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
+transporter.verify()
+  .then(() => console.log("SMTP conectado com sucesso ✅"))
+  .catch((err) => console.error("Erro no SMTP ❌", err));
 
 export const sendMail = (name, email, message) => {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
-    replyTo: email,
+    from: process.env.EMAIL_USER, 
+    to: process.env.EMAIL_USER, 
+    replyTo: email,               
     subject: `Nova mensagem de ${name}`,
     text: `Nome: ${name}\nEmail: ${email}\nMensagem:\n${message}`,
   };
+
   return transporter.sendMail(mailOptions);
 };
 
 export const sendConfirmationEmail = (userName, userEmail) => {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: userEmail,
+    from: process.env.EMAIL_USER, 
+    to: userEmail,                
     subject: 'Recebemos sua mensagem!',
     html: `
-            <p>Olá, ${userName}!</p>
-            <p>Obrigado por entrar em contato. Eu recebi sua mensagem e responderei em breve.</p>
-            <p>Atenciosamente,</p>
-            <p>Kayo Weiber</p>
-        `,
+      <p>Olá, ${userName}!</p>
+      <p>Obrigado por entrar em contato. Eu recebi sua mensagem e responderei em breve.</p>
+      <p>Atenciosamente,</p>
+      <p>Kayo Weiber</p>
+    `,
   };
+
   return transporter.sendMail(mailOptions);
 };
